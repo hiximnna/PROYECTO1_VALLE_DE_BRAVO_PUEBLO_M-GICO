3f2f476abf4acbca17283650289a89e774bdd013
hiximnna <ximelgg423@gmail.com>
2026-04-19 13:00:24 -0600

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Efecto suave al hacer clic en el menú
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
